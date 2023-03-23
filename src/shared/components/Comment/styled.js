5b7c8714dcd245561cc9a8d5305b17737b239cf6
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-03-23 00:21:37 -0400

    :sparkles: feat: Component Comentario

---
+++ src/shared/components/Comment/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container_Comment = styled.div`
+  border: 1px black solid;
+  border-radius: 8px;
+  padding: 10px;
+  margin: 0 0 20px 0;
+  .user {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 14px;
+
+    .img {
+      width: 10px;
+      height: 100px;
+      border-radius: 30%;
+      object-fit: cover;
+    }
+
+    span {
+      font-weight: bold;
+      font-size: 16px;
+    }
+    p {
+      text-align: justify;
+      line-height: 25px;
+      font-size: 15px;
+    }
+  }
+`;
