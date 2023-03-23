0458135e1c359d527adf4561c73dc184266888c1
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-03-23 09:10:38 -0400

    :sparkles: chore: Add component Header em Routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Page_Description } from "../pages/Description";
+import { Header } from "../shared/components/Header";
 
 export const Rotas = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post" element={<Page_Description />} />
