a75efaa0a68fa70f47b3609e1ea85fd4d07c3034
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-03-23 00:22:32 -0400

    :sparkles: feat: Component Header

---
+++ src/shared/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import { Code, Notepad } from "phosphor-react";
+import { Container } from "./styled";
+
+export const Header = () => {
+  return (
+    <Container>
+        <Notepad size={96} weight="duotone" />
+        <div className="title-subtitle">
+            <h1 className="title">Blog</h1>
+            <h2 className="subtitle">Making a dev happy</h2>
+        </div>
+        <Code size={96} weight="duotone" />
+    </Container>
+  );
+};
---
+++ src/shared/components/Header/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100vw;
+  height: 15rem;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background: linear-gradient(-45deg, #574ae8, #3ea1db);
+  color: #ffffff;
+
+  .title-subtitle {
+    width: 60%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    .title{
+        font-size:3rem;
+    }
+    .subtitle{
+        font-size:2rem;
+    }
+  }
+`;
