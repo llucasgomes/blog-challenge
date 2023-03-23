8c37362e5b7796e1874f8e5c1a770537f1df8cc1
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-03-23 09:17:40 -0400

    :sparkles: chore: Add component Footer

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Page_Description } from "../pages/Description";
 import { Header } from "../shared/components/Header";
+import { Footer } from "../shared/components/Footer";
 
 export const Rotas = () => {
   return (
@@ -12,6 +13,7 @@ export const Rotas = () => {
         <Route path="/post" element={<Page_Description />} />
         <Route path="*" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
