d550996eec546ae3e4b7f355af806bd8afc42764
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-03-23 09:24:27 -0400

    :lipstick: chore: Aumentando "font-size" do  span

--- src/shared/components/Footer/styled.js
+++ src/shared/components/Footer/styled.js
@@ -10,7 +10,8 @@ export const Container_Footer = styled.footer`
   align-items: center;
   justify-content: space-between;
   font-size: 12px;
-  img {
-    height: 50px;
+
+  span {
+    font-size: 20px;
   }
 `;
