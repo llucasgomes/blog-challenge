ebfe930e35ddbcff8b7cec3b1d24934d1aba2c3a
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-03-23 09:18:56 -0400

    :lipstick: chore: Pequena Alteração Footer

--- src/shared/components/Footer/styled.js
+++ src/shared/components/Footer/styled.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const Container_Footer = styled.footer`
   margin-top: 100px;
+  position: absolute;
+  width: 100%;
   padding: 20px;
   background-color: #7279f9;
   display: flex;
