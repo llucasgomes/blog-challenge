268e09e370325302297d89ce7fe440a075ff4dfc
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-03-25 20:53:33 -0400

    :sparkles: feat: Add um Loading

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,31 @@
+import { useContext } from "react";
 import { Rotas } from "../routes";
-import { DataContextProvider } from "../shared/contexts/DataContext";
+import {
+  DataContext,
+  DataContextProvider,
+} from "../shared/contexts/DataContext";
+
+import ReactLoading from "react-loading";
+import { Container_Loading } from "./styled";
 
 function App() {
+  const { isFetching } = useContext(DataContext);
+
   return (
     <DataContextProvider>
-      <Rotas />;
+      {isFetching ? (
+        <Container_Loading>
+          <ReactLoading
+            type={"spin"}
+            color={"#7279f9"}
+            height={100}
+            width={100}
+          />
+        </Container_Loading>
+      ) : (
+        <Rotas />
+      )}
+      ;
     </DataContextProvider>
   );
 }
